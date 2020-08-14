03d2d7d2bed4aa7f3ecd836a3891d3084005dd2f
Adam Snyder <snyderadam@gmail.com>
2020-08-14 17:41:03 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import Welcome from './components/welcome/Welcome'
+import Clock from './components/clock/Clock'
+import Contact from './components/contact/Contact'
+import {Route} from 'react-router-dom'
+import Navigation from './components/navigation/Navigation'
+import {Switch} from 'react-router-dom'
+import './App.css'
+
+const errorPage = () =>{
+  return ( <h3>
+    'ERROR 404 PAGE NOT FOUND' </h3>)
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+      <Route exact path = '/welcome/:name' 
+      render = {(props)=> <Welcome {...props}name = {props.match.params.name}/>}/>
+      <Route exact path = '/' 
+      render = {(props)=> <Welcome {...props}name = 'Adam'/>}/>
+      <Route path = '/clock' component = {Clock}/>
+      <Route path = '/contact' component = {Contact}/>
+      <Route component = {errorPage}/>
+      </Switch>
+      <Navigation/>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
