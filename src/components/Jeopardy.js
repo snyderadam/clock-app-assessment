455f1c2de00379d1f8f79dafc52e2a4756e9c012
Adam Snyder <snyderadam@gmail.com>
2020-08-27 17:30:04 -0400

    midway2

--- src/components/Jeopardy.js
+++ src/components/Jeopardy.js
@@ -1,42 +1,75 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 //import our service
 import JeopardyService from "../jeopardyService";
 class Jeopardy extends Component {
   //set our initial state and set up our service as this.client on this component
-  constructor(props){
+  constructor(props) {
     super(props);
     this.client = new JeopardyService();
     this.state = {
-      data: {category: {}},
+      data: { category: {} },
+      formData: {
+        answer: "",
+      },
       score: 0,
-      formData: ""
-    }
+    };
   }
   //get a new random question from the API and add it to the data object in state
   getNewQuestion() {
-    return this.client.getQuestion().then(result => {
+    return this.client.getQuestion().then((result) => {
       this.setState({
-        data: result.data[0]
-      })
-    })
+        data: result.data[0],
+      });
+      console.log(this.state.data.answer);
+    });
   }
   //when the component mounts, get a the first question
   componentDidMount() {
     this.getNewQuestion();
   }
-  //display the results on the screen
+  handleChange = (event) => {
+    let formData = this.state.formData;
+    formData[event.target.name] = event.target.value;
+    this.setState({ formData });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    if (this.state.formData.answer === this.state.data.answer) {
+      return this.setState({
+        score: this.state.score + this.state.data.value,
+      });
+    } else {
+      return this.setState({
+        score: this.state.score - this.state.data.value,
+      });
+    }
+  };
   render() {
     return (
       <div>
-    <strong>Question: </strong>{this.state.data.question}<br/>
-    <strong>Points: </strong>{this.state.data.value}<br/>
-    <strong>Category: </strong>{this.state.data.category.title}<br/>
-    <form>
-        <label>Answer: </label>
-        <input className = 'inputBox'></input>
-        <button>Submit</button>
-    </form>
-    <h3>Score: {this.state.score}</h3>
+        <strong>Question: </strong>
+        {this.state.data.question}
+        <br />
+        <strong>Points: </strong>
+        {this.state.data.value}
+        <br />
+        <strong>Category: </strong>
+        {this.state.data.category.title}
+        <br />
+        <form onSubmit={this.handleSubmit}>
+          <label>Answer: </label>
+          <input
+            onChange={this.handleChange}
+            type="text"
+            name="answer"
+            value={this.state.formData.answer}
+          />
+          <br/>
+          <button>Submit</button>
+          <br />
+        </form>
+        <h3>Score: {this.state.score}</h3>
       </div>
     );
   }
