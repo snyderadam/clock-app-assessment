6f9d06e2abe8540fde91937963b3a0238907d05b
Adam Snyder <snyderadam@gmail.com>
2020-08-28 16:32:03 -0400

    midway3

--- src/components/jeopardy/Jeopardy.js
+++ src/components/jeopardy/Jeopardy.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-//import our service
 import JeopardyService from "../../jeopardyService";
 import Display from '../display/Display'
 class Jeopardy extends Component {
